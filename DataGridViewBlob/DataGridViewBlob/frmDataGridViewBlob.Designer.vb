02f7c236521408f3eb1fa647ad37443c6c5a1ca7
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2021-10-09 12:45:25 +0700

    Add files via upload

---
+++ DataGridViewBlob/DataGridViewBlob/frmDataGridViewBlob.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDataGridViewBlob
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmDataGridViewBlob))
+        Me.dgvData = New System.Windows.Forms.DataGridView()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.lblRecordCount = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvData
+        '
+        Me.dgvData.AllowUserToAddRows = False
+        Me.dgvData.AllowUserToDeleteRows = False
+        Me.dgvData.AllowUserToResizeRows = False
+        Me.dgvData.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvData.BackgroundColor = System.Drawing.Color.Gainsboro
+        Me.dgvData.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvData.Location = New System.Drawing.Point(1, 36)
+        Me.dgvData.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.dgvData.MultiSelect = False
+        Me.dgvData.Name = "dgvData"
+        Me.dgvData.ReadOnly = True
+        Me.dgvData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvData.Size = New System.Drawing.Size(912, 566)
+        Me.dgvData.TabIndex = 2
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnRefresh.Image = CType(resources.GetObject("btnRefresh.Image"), System.Drawing.Image)
+        Me.btnRefresh.Location = New System.Drawing.Point(201, 5)
+        Me.btnRefresh.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(24, 24)
+        Me.btnRefresh.TabIndex = 1
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'lblRecordCount
+        '
+        Me.lblRecordCount.AutoSize = True
+        Me.lblRecordCount.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblRecordCount.Location = New System.Drawing.Point(227, 11)
+        Me.lblRecordCount.Name = "lblRecordCount"
+        Me.lblRecordCount.Size = New System.Drawing.Size(15, 13)
+        Me.lblRecordCount.TabIndex = 63
+        Me.lblRecordCount.Text = "[]"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label12.ForeColor = System.Drawing.Color.Black
+        Me.Label12.Location = New System.Drawing.Point(5, 9)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(38, 14)
+        Me.Label12.TabIndex = 64
+        Me.Label12.Text = "ค้นหา"
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtSearch.Location = New System.Drawing.Point(50, 6)
+        Me.txtSearch.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(151, 22)
+        Me.txtSearch.TabIndex = 0
+        '
+        'frmDataGridViewBlob
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(916, 603)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Controls.Add(Me.lblRecordCount)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.dgvData)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "frmDataGridViewBlob"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "DataGridView with BLOB - Code By: Thongkorn Tubtimkrob"
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents dgvData As System.Windows.Forms.DataGridView
+    Friend WithEvents btnRefresh As System.Windows.Forms.Button
+    Friend WithEvents lblRecordCount As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+
+End Class
---
+++ DataGridViewBlob/DataGridViewBlob/frmDataGridViewBlob.vb
@@ -0,0 +1,209 @@
+﻿#Region "ABOUT"
+' / ---------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gnet.com/webboard
+' /
+' / Purpose: Sample code to retrieve data to show in DataGridView.
+' / Microsoft Visual Basic .NET (2010) + MS Access 2010+
+' /
+' / This is open source code under @CopyLeft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / ---------------------------------------------------------------
+#End Region
+
+Imports System.Data.OleDb
+
+Public Class frmDataGridViewBlob
+    Dim Conn As OleDb.OleDbConnection
+    Dim DA As New System.Data.OleDb.OleDbDataAdapter()
+    Dim DR As OleDbDataReader
+    Dim Cmd As New System.Data.OleDb.OleDbCommand
+    Dim DT As New DataTable
+    Dim strSQL As String
+
+    '// Connect MS Access DataBase
+    Function ConnectDataBase() As OleDb.OleDbConnection
+        Return New OleDb.OleDbConnection( _
+            "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & _
+            MyPath(Application.StartupPath) & "data\" & "Countries.accdb;Persist Security Info=True")
+    End Function
+
+    ' / --------------------------------------------------------------------------------
+    ' / Get my project path
+    Function MyPath(ByVal AppPath As String) As String
+        '/ MessageBox.Show(AppPath);
+        MyPath = AppPath.ToLower.Replace("\bin\debug", "\").Replace("\bin\release", "\").Replace("\bin\x86\debug", "\")
+        '/ Return Value
+        '// If not found folder then put the \ (BackSlash) at the end.
+        If Microsoft.VisualBasic.Right(MyPath, 1) <> Chr(92) Then MyPath = MyPath & Chr(92)
+    End Function
+
+    Private Sub frmDataGridViewBlob_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Conn = ConnectDataBase()
+        Call SetupDataGridView(dgvData)
+        Call GetDataTable()
+    End Sub
+
+    Private Sub SetupDataGridView(ByRef DGV As DataGridView)
+        With DGV
+            .Columns.Clear()
+            .RowTemplate.Height = 26
+            .AllowUserToOrderColumns = True
+            .AllowUserToDeleteRows = False
+            .AllowUserToAddRows = False
+            .ReadOnly = True
+            .MultiSelect = False
+            .SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            .Font = New Font("Tahoma", 9)
+            '// Even-Odd Color
+            .AlternatingRowsDefaultCellStyle.BackColor = Color.OldLace
+            .DefaultCellStyle.SelectionBackColor = Color.SeaGreen
+            .ReadOnly = True
+            .MultiSelect = False
+            .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            '// Header Styles
+            With .ColumnHeadersDefaultCellStyle
+                .BackColor = Color.Navy
+                .ForeColor = Color.Black
+                .Font = New Font("Tahoma", 10, FontStyle.Bold)
+            End With
+            '//
+            Dim CountryPK As New DataGridViewTextBoxColumn
+            With CountryPK
+                .DataPropertyName = "CountryPK"
+                .HeaderText = "CountryPK"
+                '// Hidden Column.
+                .Visible = False
+            End With
+            .Columns.Add(CountryPK)
+            '//
+            Dim Flag As New DataGridViewImageColumn
+            With Flag
+                .DataPropertyName = "Flag"
+                .HeaderText = "Flag"
+                .ImageLayout = DataGridViewImageCellLayout.Stretch
+                .AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+            End With
+            .Columns.Add(Flag)
+            '//
+            Dim A2 As New DataGridViewTextBoxColumn
+            With A2
+                .DataPropertyName = "A2"
+                .HeaderText = "A2"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+            End With
+            .Columns.Add(A2)
+            '//
+            Dim Country As New DataGridViewTextBoxColumn
+            With Country
+                .DataPropertyName = "Country"
+                .HeaderText = "Country"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+            End With
+            .Columns.Add(Country)
+            '//
+            Dim Capital As New DataGridViewTextBoxColumn
+            With Capital
+                .DataPropertyName = "Capital"
+                .HeaderText = "Capital"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+            End With
+            .Columns.Add(Capital)
+            '//
+            Dim ZoneName As New DataGridViewTextBoxColumn
+            With ZoneName
+                .DataPropertyName = "ZoneName"
+                .HeaderText = "ZoneName"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+            End With
+            .Columns.Add(ZoneName)
+
+            Dim Population As New DataGridViewTextBoxColumn
+            With Population
+                .DataPropertyName = "Population"
+                .HeaderText = "Population"
+                .DefaultCellStyle.Format = "N0"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+            End With
+            .Columns.Add(Population)
+        End With
+    End Sub
+
+    '// การค้นหาข้อมูล หรือแสดงผลข้อมูลทั้งหมด จะใช้เพียงโปรแกรมย่อยแบบ Sub Program ชุดเดียว
+    '// หากค่าที่ส่งมาเป็น False (หรือไม่ส่งมา จะถือเป็นค่า Default) นั่นคือให้แสดงผลข้อมูลทั้งหมด
+    '// หากค่าที่ส่งมาเป็น True จะเป็นการค้นหาข้อมูล
+    Private Sub GetDataTable(Optional ByVal blnSearch As Boolean = False)
+        strSQL = _
+            " SELECT CountryPK, Flag, A2, Country, Capital, Population, Zones.ZoneName " & _
+            " FROM Countries INNER JOIN Zones ON Countries.ZoneFK = Zones.ZonePK "
+        '// หากส่งค่า True มาจะเป็นการค้นหา
+        If blnSearch Then
+            strSQL = strSQL & _
+                    " WHERE " & _
+                    " [A2] " & " Like '%" & Trim(txtSearch.Text) & "%'" & " OR " & _
+                    " [Country] " & " Like '%" & Trim(txtSearch.Text) & "%'" & " OR " & _
+                    " [Capital] " & " Like '%" & Trim(txtSearch.Text) & "%'" & " OR " & _
+                    " [ZoneName] " & " Like '%" & Trim(txtSearch.Text) & "%'"
+            '// Else ไม่ต้องมี
+        End If
+        '// เอา strSQL มาเรียงต่อกัน
+        strSQL = strSQL & " ORDER BY Countries.A2 "
+        '/
+        If Conn.State = ConnectionState.Closed Then Conn.Open()
+        Try
+            Cmd = Conn.CreateCommand
+            Cmd.CommandText = strSQL
+            DT = New DataTable
+            DA = New OleDbDataAdapter(Cmd)
+            DA.Fill(DT)
+            dgvData.DataSource = DT
+            lblRecordCount.Text = "[จำนวน : " & DT.Rows.Count & " รายการ.]"
+            DT.Dispose()
+            DA.Dispose()
+            txtSearch.Text = ""
+            txtSearch.Focus()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub txtSearch_KeyPress(sender As Object, e As System.Windows.Forms.KeyPressEventArgs) Handles txtSearch.KeyPress
+        If Trim(txtSearch.Text.Trim) = "" Or Len(Trim(txtSearch.Text.Trim)) = 0 Then Return
+        '// RetrieveData(True) It means searching for information.
+        If e.KeyChar = Chr(13) Then '// Press Enter
+            '// No beep.
+            e.Handled = True
+            '// Undesirable characters for the database.
+            txtSearch.Text = txtSearch.Text.Trim.Replace("'", "").Replace("%", "").Replace("*", "")
+            '// Send True Value is Search Data.
+            Call GetDataTable(True)
+        End If
+    End Sub
+
+    Private Sub btnRefresh_Click(sender As System.Object, e As System.EventArgs) Handles btnRefresh.Click
+        Call GetDataTable(True)
+    End Sub
+
+    Private Sub dgvData_CellDoubleClick(sender As Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgvData.CellDoubleClick
+        If dgvData.RowCount = 0 Then Return
+        '// Read the value of the focus row.
+        Dim iRow As Integer = dgvData.CurrentRow.Index
+        MessageBox.Show( _
+            "Primary key : " & dgvData.Item(0, iRow).Value & vbCrLf & _
+            "Country : " & dgvData.Item(3, iRow).Value & vbCrLf & _
+            "Capital is : " & dgvData.Item(4, iRow).Value)
+    End Sub
+
+    Private Sub frmDataGridViewBlob_FormClosed(sender As Object, e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        If Conn.State = ConnectionState.Open Then Conn.Close()
+        Me.Dispose()
+        GC.SuppressFinalize(Me)
+        Application.Exit()
+    End Sub
+
+End Class
